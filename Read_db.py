7176bf15a46b3155066e5231e9ad0cb3ab76629d
KongBoje <Michael_veilis@hotmail.com>
2018-02-05 10:50:07 +0100

    Rettede en lille fejl i Read.

--- Read_db.py
+++ Read_db.py
@@ -4,8 +4,8 @@ input_file = open("myNumberFile.db", "rb")
 
 numberDict = pickle.load(input_file)
 
-for key, value in numberDict.iteritems():
-        print 'Key:', key, 'Value:', numberDict[key]
+for key, value in numberDict.items():
+        print('Key:', key, 'Value:', numberDict[key])
 
 #print 'read 1st key and value:', numberDict.items()[0]
 
