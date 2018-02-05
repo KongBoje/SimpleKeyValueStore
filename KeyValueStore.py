0ebad5695d0c259015e8298ca215738fd36c0cf6
KongBoje <Michael_veilis@hotmail.com>
2018-02-05 18:37:41 +0100

    Lavet write og read til funktioner i en main funktion

---
+++ KeyValueStore.py
@@ -0,0 +1,38 @@
+import pickle
+
+def main():
+
+    def write():
+        output_file = open("myNumberFile.db", "wb")
+
+        numberDict = {
+            "1": "One",
+            "2": "Two",
+            "3": "Three",
+            "4": "Four",
+            "5": "Five",
+            "6": "Six"
+        }
+
+        pickle.dump(numberDict, output_file)
+
+        output_file.close()
+
+    def read():
+        input_file = open("myNumberFile.db", "rb")
+
+        numberDict = pickle.load(input_file)
+
+        for key, value in numberDict.items():
+                print('Key:', key, 'Value:', numberDict[key])
+
+        #print 'read 1st key and value:', numberDict.items()[0]
+        print('Number of elements in the db file:', len(numberDict))
+
+        input_file.close()
+    
+    write()
+    read()
+
+if __name__ == "__main__":
+    main()
